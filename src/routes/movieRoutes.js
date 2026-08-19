680197dcc7d12dec35b446e7eef47fc303b0d26a
thenullpointerdev-ops <thenullpointer.dev@gmail.com>
2026-08-19 21:38:39 +0300

    Implement CRUD routes for movie management
    Added CRUD routes for movies with appropriate HTTP methods.

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -1,1 +1,31 @@
+import express from "express"; // we change in package.json "type": "module" to use import instead of require
+
+const router = express.Router(); // create an instance of express router
+
+router.get("/", (req, res) => {
+    res.json({httpMethod: "get"});
+});
+
+// creating data : creating user - adding movie to the database
+router.post("/", (req, res) => {
+    res.json({httpMethod: "post"});
+});
+
+// updating data : updating user's email - updating movie in the database
+router.put("/", (req, res) => {
+    res.json({httpMethod: "put"});
+});
+
+// removing stuff
+router.delete("/", (req, res) => {
+    res.json({httpMethod: "delete "});
+});
+
+
+
+export default router; // export the router to be used in other files
+
+
+
+
 

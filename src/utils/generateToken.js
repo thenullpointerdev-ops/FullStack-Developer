5091c482daf0e8457e10c850c7f3a4b09e7dee32
thenullpointerdev-ops <thenullpointer.dev@gmail.com>
2026-08-19 21:36:57 +0300

    Update generateToken.js

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,1 +1,19 @@
+import jwt from "jsonwebtoken";
 
+export const generateToken = (userId, res) => {
+    const payload = { id: userId };
+
+    // Transform it to string of letters & numbers, but when you convert back it will be an object.
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    });
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        samesite: "strict", // stop sending cookie on cross-site requests
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    })
+    return token;
+}
